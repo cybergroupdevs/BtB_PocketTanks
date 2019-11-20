6a8c3f509ef98a10ecf8001f3b18602f229baa5f
sahil97 <sahil.sharma071997@gmail.com>
2019-11-20 20:53:39 +0530

    added response wrapper to its own folder

--- ms-machine-learning/utils/response_wrapper/__init__.py
+++ ms-machine-learning/utils/response_wrapper/__init__.py
@@ -1,7 +1,7 @@
 # Response wrapper to send uniform responses from the microservices
 from flask import jsonify
 
-class response_wrapper():
+class Response_wrapper():
     def create_response(status, data, message=""):
         return jsonify({
             "status" : status,

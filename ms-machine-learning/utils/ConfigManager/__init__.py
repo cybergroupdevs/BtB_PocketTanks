2cbb9afac00ddc5b5663fa58ea0406135b4046c2
sahil97 <sahil.sharma071997@gmail.com>
2019-12-02 17:53:29 +0530

    added docstring comments for documentation purposes

--- ms-machine-learning/utils/ConfigManager/__init__.py
+++ ms-machine-learning/utils/ConfigManager/__init__.py
@@ -2,6 +2,7 @@ import os
 import json
 from werkzeug.contrib.fixers import ProxyFix
 
+''' Reading global config to use in config manager. '''
 try:
     with open(os.path.join(os.path.dirname(__file__),'../../../config.json')) as config_file:
         config = json.load(config_file)
@@ -10,6 +11,7 @@ except FileNotFoundError as e:
             config = json.load(config_file)
 
 class PrefixMiddleware(object):
+    ''' Class to add url prefix to this microservice and reject all other request from the entry file. '''
     def __init__(self, app, prefix=''):
         self.app = app
         self.prefix = prefix
@@ -24,6 +26,7 @@ class PrefixMiddleware(object):
             return ["This url does not belong to the app.".encode()]
 
 def setAppConfig(app):
+    ''' Method to set the initial config variables on basis of global config for all microservices '''
     app.config['ENGINE_NAME'] = 'ML'
     app.config['ENV'] = 'DEV'
     app.config['IP'] = config[app.config['ENV']][app.config['ENGINE_NAME']]['IP']

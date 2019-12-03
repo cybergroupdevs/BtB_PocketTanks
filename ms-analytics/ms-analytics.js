77bf86ac8ae8cf436bea0f5684a750a109f0dcc5
sahil97 <sahil.sharma071997@gmail.com>
2019-12-03 19:50:51 +0530

    removed unused require syntax

--- ms-analytics/ms-analytics.js
+++ ms-analytics/ms-analytics.js
@@ -3,60 +3,55 @@
 
 import express from 'express';
 import expressSession from 'express-session';
-import bodyParser  from 'body-parser'
-// const express = require('express');
-// const expressSession = require('express-session');
-// const bodyParser = require('body-parser');
-import globalConfig from '../config.json'
-// const globalConfig = require('../config.json');
-// const routes = require("./src/routes");
-import routes from './src/routes'
-import db from './app/db/mongoose'
+import bodyParser from 'body-parser';
+import globalConfig from '../config.json';
+import routes from './src/routes';
+import db from './app/db/mongoose';
 const app = express();
 
 // Required for twitter OAuth
 app.use(expressSession({
-  secret: 'secretKeyFromEnv'
+    secret: 'secretKeyFromEnv'
 }));
 
 // Enabling CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, x-auth,Authorization, Content-Type, Accept"
-  );
-  res.header(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, PATCH , DELETE, OPTIONS"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, x-auth,Authorization, Content-Type, Accept"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH , DELETE, OPTIONS"
+    );
+    next();
 });
 
 // Body parser config
 app.use(bodyParser.json({
-  limit: "50mb"
+    limit: "50mb"
 }));
 app.use(bodyParser.urlencoded({
-  limit: "50mb",
-  extended: true
+    limit: "50mb",
+    extended: true
 }));
 
 const port =
-  process.env.PORT ||
-  globalConfig[process.env.ENV][process.env.ENGINE_NAME]["PORT"];
+    process.env.PORT ||
+    globalConfig[process.env.ENV][process.env.ENGINE_NAME]["PORT"];
 
 app.get('/', (req, res) => res.send('Welcome to the Pocket Tanks'));
 
 const server = app.listen(port, () => {
-  console.log(`Microservice running on PORT : ${port}`);
-  console.log("Current Environment : ", process.env.ENV);
-  console.log("Base for API", routes.version.apiBaseUri);
+    console.log(`Microservice running on PORT : ${port}`);
+    console.log("Current Environment : ", process.env.ENV);
+    console.log("Base for API", routes.version.apiBaseUri);
 });
 app.use(routes.version.apiBaseUri, routes.version.api(app));
 process.on("uncaughtException", function(err) {
-  console.error(new Date().toUTCString() + " uncaughtException:", err.message);
-  console.error(err.stack);
-  return false;
-  process.exit(1);
+    console.error(new Date().toUTCString() + " uncaughtException:", err.message);
+    console.error(err.stack);
+    return false;
+    process.exit(1);
 });

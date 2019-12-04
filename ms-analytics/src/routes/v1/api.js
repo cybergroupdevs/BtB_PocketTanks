35b46c0286cdd0bf5869bed22bcfa57e71837951
sahil97 <sahil.sharma071997@gmail.com>
2019-12-04 13:58:56 +0530

    added twitter oAuth routes

--- ms-analytics/src/routes/v1/api.js
+++ ms-analytics/src/routes/v1/api.js
@@ -1,10 +1,15 @@
-import  express  from 'express';
-import api  from '../../controllers';
+import express from 'express';
+import api from '../../controllers';
+import PassportTwt from '../../middlewares/passport-twitter';
 
 let apiRoutes = (router) => {
     router = express.Router();
-    router.post('/registration', api.users.registration)
-    router.post('/login', api.users.login)
+    router.post('/registration', api.users.registration);
+    router.post('/login', api.users.login);
+
+    // Routes for OAuth
+    router.get('/auth/twitter', PassportTwt.authenticate('twitter'));
+    router.post('/auth/twitter/extracttokens', api.auth.twitter);
     return router;
 }
 export default apiRoutes;

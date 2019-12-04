b297fea5e7bba2b03ba2996ebb0b4fac9e6f9a1f
Nitesh Gautam <niteshpandey31@gmail.com>
2019-12-04 18:50:50 +0530

    Add Email Wrapper

---
+++ ms-analytics/wrappers/mailer/mailer.js
@@ -0,0 +1,30 @@
+import nodemailer from 'nodemailer';
+
+class Mailer {
+    constructor(){
+        this.transporter = nodemailer.createTransport({
+            service: "gmail",
+            host: "smtp.gmail.com",
+            secureConnection: true,
+            auth: {
+                user: "pockettanks60@gmail.com",
+                pass: "Pockettanks@60"
+            }
+        });
+    }
+    async sendEmail(to, subject, html){
+        try{
+            let message = {
+                from: "no-reply@pocettanks.com",
+                to: to,
+                subject: subject,
+                html: html
+            }
+            await this.transporter.sendEmail(message)
+        }
+        catch(error){
+            throw new Error(error.message);
+        }
+    }
+}
+export default Mailer;

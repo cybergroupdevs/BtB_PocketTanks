bbee77fb8cf6afd8171ffec34227fd7a5c27ebd3
neerajCYG <neeraj211090@yahoo.com>
2019-12-09 13:54:52 +0530

    Merging conflicts

--- ms-analytics/src/routes/v1/api.js
+++ ms-analytics/src/routes/v1/api.js
@@ -2,19 +2,22 @@
 import express from 'express';
 import api from '../../controllers';
 import PassportTwt from '../../middlewares/passport-twitter';
+import Validation from '../../middlewares/validation';
 
 
+
+const validation = new Validation()
 
 let apiRoutes = (router) => {
     router = express.Router();
 
-    router.post('/registration', api.users.registration);
-    router.post('/login', api.users.login);
-    router.post('/forgotpassword', api.users.forgotPassword);
-    router.post('/forgotPasswordChanged', api.users.forgotPasswordChanged);
-    router.post('/changePassword', api.users.changePassword);
-    router.post('/emailVerification', api.users.emailVerification);
-
+    router.post('/registration', validation.registration, api.users.registration);
+    router.post('/login', validation.login, api.users.login);
+    router.post('/forgotpassword', validation.forgotPassword, api.users.forgotPassword);
+    router.post('/forgotPasswordChanged', validation.forgotPasswordChanged, api.users.forgotPasswordChanged);
+    router.post('/changePassword', validation.changePassword, api.users.changePassword);
+    router.post('/emailVerification',validation.emailVerification, api.users.emailVerification);
+    
     // Routes for OAuth
     router.get('/auth/twitter', PassportTwt.authenticate('twitter'));
     router.post('/auth/twitter/extracttokens', api.auth.twitter);
